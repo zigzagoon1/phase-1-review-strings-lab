6fbbb3516a185d038782f91bd9f6059ee3faaf2b
KellyMark <kmark602@gmail.com>
2022-12-12 11:30:56 -0500

    commit tree 2feb672152b3b73a2d7f9c37dd053ba40a6c34c1

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = "Grace Hopper";
+
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
+
+const shortGreeting = "Welcome, " + currentUser[0] + "!";
